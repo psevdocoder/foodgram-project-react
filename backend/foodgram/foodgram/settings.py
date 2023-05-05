098e705530e8f9c2d53ab5aad3317744660e265b
Maksim Kautarov <just5py@yandex.ru>
2023-05-05 07:30:28 +0300

    hope last

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG_STATE', default=False, cast=bool)
+DEBUG = bool(os.getenv('DEBUG_STATE', default=False))
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '85.234.110.243']
 
