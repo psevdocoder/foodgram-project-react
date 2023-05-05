ae2ddcc8e3fdd24b01e5ddb64424841e0f4edd36
Maksim Kautarov <just5py@yandex.ru>
2023-05-05 07:49:06 +0300

    hope last

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -17,7 +17,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
-
 from foodgram import settings
 
 urlpatterns = [
